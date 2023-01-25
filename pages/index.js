9eaf335949da0f79e16b9384909372c55212681a
Gabriel Lobo <lofgrenlobo@gmail.com>
2023-01-24 21:38:25 -0300

    Mudanças no texto de introdução

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import Head from 'next/head'
+import Link from 'next/link'
 import Main from '../layout/Main'
 import styles from '../styles/Home.module.scss'
 import { FaLinkedin, FaInstagram, FaGithub } from 'react-icons/fa'
 import { MdMail } from 'react-icons/md'
+import moment from 'moment'
 export default function Home() {
 	return (
 		<Main>
@@ -10,25 +12,19 @@ export default function Home() {
 				<title>Gabriel Lobo</title>
 			</Head>
 			<h1>Olá.</h1>
-			<p>Sou Gabriel Lobo - estudante de engenharia elétrica
-				atualmente trabalhando como desenvolvedor na <a href="https://norus.com.br">Norus </a>
-				em Florianópolis.
+			<p>Bem-vindo ao site do meu portfólio pessoal!</p>
+			<p> Meu nome é Gabriel Lobo e sou engenheiro eletricista 
+				com experiência em desenvolvimento de software. Tenho paixão por criar tecnologias inovadoras e 
+				trabalho como desenvolvedor de software há {moment().diff(moment([2019,1,1]), 'years')} anos. 
+				Tenho experiência em várias linguagens de programação, como JavaScript, Python e C# e atualmente 
+				trabalho como desenvolvedor na <a href="https://norus.com.br">Norus </a>
+				em Florianópolis. Também tive a oportunidade de trabalhar com previsão de energia eólica no 
+				laboratório <a href='https://www.lepten.ufsc.br/'> LEPTEN/Boiling </a> na <a href='https://ufsc.br/'>UFSC</a>.
 			</p>
-			<p>Tenho grande interesse por desenvolvimento web, com
-				ênfase em bibiliotecas de JavaScript e no ecossitema ASP.NET.
-				Você pode encontrar alguns dos meus projetos na aba
-				<a href='/portfolio'> portfolio</a> ou na minha página
-				<a href="https://githhub.com/gabbelobo"> Github</a>.
-			</p>
-			<p>A geração eólica é outra área em que tenho experiência,
-				tendo feito iniciação científica no laboratório
-				<a href="https://www.lepten.ufsc.br/"> LEPTEN/Boiling </a>
-				na <a href="https://ufsc.br/">UFSC</a>. Lá trabalhamos com
-				sistema numérico de previsões atmosféricas
-				<a href="https://www.mmm.ucar.edu/weather-research-and-forecasting-model"> WRF </a>
-				para fazer a análise de sensibilidade para previsão de
-				geração eólica. Também estou escrevendo meu TCC nesse mesmo tema.
-			</p>
+			<p>Neste site, você encontrará uma <a href='/portfolio'> coletânea do meu trabalho </a>
+				 e da minha <Link href={'/experiencia'}>experiência profissional</Link>. 
+				Sinta-se à vontade para dar uma olhada e entre em contato comigo se tiver alguma dúvida.</p>
+			
 			<div className={styles.socials}>
 				<a href="https://www.linkedin.com/in/gabbelobo/" aria-label='Gabriels linkedin page'>
 					<FaLinkedin size={'2rem'} />
